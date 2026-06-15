109879f51d2432ef20c0d8bbbacb3c40ac1cd999
Jesé <jesehernandezj@gmail.com>
2026-06-15 10:54:06 +0100

    items realizado

---
+++ src/app/interfaces/item-filters.ts
@@ -0,0 +1,7 @@
+export interface ItemFilters {
+    search?: string;
+    category?: number;
+    community?: number;
+    minPrice?: number;
+    maxPrice?: number;
+}
---
+++ src/app/interfaces/item.ts
@@ -0,0 +1,11 @@
+export interface Item {
+    id: number;
+    name: string;
+    price: number;
+    stock: number;
+    description: string;
+    image: string;
+    status: number;
+    category_id: number;
+    community_id: number;
+}
